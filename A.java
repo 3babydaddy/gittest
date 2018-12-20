5d62475cc9a745a9549d1e487340d2f10f5ba69e
wanghaiwang <wanghawang@cebbank.com>
2018-12-20 21:20:16 +0800

    test

--- A.java
+++ A.java
@@ -7,4 +7,10 @@ public static void main(String[] args){
 	System.out.println("BBBBBB");
 	System.out.println("BBBBBB");
 	
+	System.out.println("AAAAAA");
+	System.out.println("AAAAAA");
+	System.out.println("AAAAAA");
+	System.out.println("AAAAAA");
+	System.out.println("AAAAAA");
+	System.out.println("AAAAAA");
 }
